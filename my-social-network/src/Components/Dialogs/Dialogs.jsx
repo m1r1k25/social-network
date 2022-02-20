101ff362201a080d6f17da31100cb787d500f60b
m1r1k25@mail.ru <m1r1k25@mail.ru>
2022-02-20 16:53:01 +0300

    feat: update dialog component

--- my-social-network/src/Components/Dialogs/Dialogs.jsx
+++ my-social-network/src/Components/Dialogs/Dialogs.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
   return (
-    <div>
-      Dialogs
-    </div>
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog}>
+          John
+        </div>
+        <div className={s.dialog}>
+          Anna
+        </div>
+        <div className={s.dialog}>
+          Andy
+        </div>
+        <div className={s.dialog}>
+          Scott
+        </div>
+        <div className={s.dialog}>
+          Jessica
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>Hello</div>
+        <div className={s.message}>What's up</div>
+      </div>
+    </div >
   )
 }
 
