dd99f33a02dfcf38743be22a89a8aba5e698815f
m1r1k25@mail.ru <m1r1k25@mail.ru>
2022-02-27 23:49:08 +0300

    feat: add users component

--- my-social-network/src/App.js
+++ my-social-network/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Profile from './Components/Profile/Profile';
 import { Routes, Route } from 'react-router-dom';
 import DialogsContainer from './Components/Dialogs/DialogsContainer';
+import UsersContainer from './Components/Users/UsersContainer';
 
 const App = (props) => {
   return (
@@ -15,7 +16,7 @@ const App = (props) => {
         <Routes>
           <Route path='/dialogs/*' element={<DialogsContainer store={props.store} />} />
           <Route path='/profile' element={<Profile store={props.store} />} />
-          <Route path='/users/' element={() => <div>users</div>} />
+          <Route path='/users/' element={<UsersContainer store={props.store} />} />
         </Routes>
       </div>
     </div>
