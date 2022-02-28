3266c664ed6757d12d6573569b9a852047e67c40
Miroslav Zaitsev <m1r1k25@mail.ru>
2022-02-28 11:25:20 +0300

    feat: start using axios

--- my-social-network/src/Components/Users/Users.jsx
+++ my-social-network/src/Components/Users/Users.jsx
@@ -1,42 +1,26 @@
 import React from 'react';
-import styles from './users.module.css'
+import styles from './users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../../src/assets/images/i.webp';
 
 const Users = (props) => {
-  if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        photoUrl: 'https://i.pinimg.com/originals/7c/c7/a6/7cc7a630624d20f7797cb4c8e93c09c1.png',
-        followed: false,
-        fullName: 'Dmitry',
-        status: 'I am a boss',
-        location: { city: 'Minsk', country: 'Belarus' }
-      },
-      {
-        id: 2,
-        photoUrl: 'https://i.pinimg.com/originals/7c/c7/a6/7cc7a630624d20f7797cb4c8e93c09c1.png',
-        followed: true,
-        fullName: 'Sasha',
-        status: 'I am a boss too',
-        location: { city: 'Moscow', country: 'Russia' }
-      },
-      {
-        id: 3,
-        photoUrl: 'https://i.pinimg.com/originals/7c/c7/a6/7cc7a630624d20f7797cb4c8e93c09c1.png',
-        followed: false,
-        fullName: 'Andrew',
-        status: 'I am a boss too',
-        location: { city: 'Kiev', country: 'Ukraine' }
-      }
-    ]
-    )
+  const getUsers = () => {
+    if (props.users.length === 0) {
+      axios
+        .get('https://social-network.samuraijs.com/api/1.0/users')
+        .then(response => {
+          props.setUsers(response.data.items)
+        })
+    }
   }
+
   return (
     <div>
+      <button onClick={getUsers}>Get users</button>
       {props.users.map(u => <div key={u.id}>
         <span>
           <div>
-            <img src={u.photoUrl} className={styles.userPhoto} />
+            <img src={u.photos.small !== null ? u.photos.small : userPhoto} className={styles.userPhoto} />
           </div>
           <div>
             {u.followed
@@ -49,12 +33,12 @@ const Users = (props) => {
           </div>
         </span>
         <span>
-          <div>{u.fullName}</div>
+          <div>{u.name}</div>
           <div>{u.status}</div>
         </span>
         <span>
-          <div>{u.location.country}</div>
-          <div>{u.location.city}</div>
+          <div>{'u.location.country'}</div>
+          <div>{'u.location.city'}</div>
         </span>
       </div >)}
     </div >
